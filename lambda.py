0709707ae9d5a17b3afb57df8f2c89df3f393d26
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-04 19:28:51 +0800

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,15 @@
+x = lambda a, b ,c  : a+b+c
+print(x(1,2,3))
+
+
+
+def myfunction (x):
+    return lambda n : n *x 
+
+double = myfunction(2)
+
+print("hi", double(11))
+
+
+product = lambda a, b=1 , c=2 :a+b+c
+print(product(3))
