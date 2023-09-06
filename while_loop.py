f35c2159d83fda97c1086d904cac4d6d89ff6aea
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-06 18:09:52 +0800

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+x = 1 
+while x<=10:
+    print(x)
+    x+=1
+
+
+y = 1
+
+while y <10:
+    print(y)
+    y+=1
+    if y == 6:
+        break
+
+z = 1
+while z <= 10:
+    print(z)
+    z+=1
+else:
+    print("done")
+
