723ec966af5686769e066d4bad2bada2d6acb437
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-06 18:09:28 +0800

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,11 @@
+tuple = ("a", "b", "c", "d")
+ntuple =("a") # no ","
+numtuple = (1,2,3,4,5)
+
+print(tuple)
+print(len(tuple))
+print(numtuple)
+
+print(type(tuple))
+print(type(ntuple))
+print(type(numtuple))
