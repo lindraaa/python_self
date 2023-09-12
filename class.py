54821b2cb0e0a26ee53841e685a6fa50225c9b1f
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-12 17:04:43 +0800

    Create class.py

---
+++ class.py
@@ -0,0 +1,25 @@
+class Car:
+    def __init__(self,make,model,year,color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+        
+    def drive(self):
+        print("The car is driving")
+    def stop(self):
+        print("The car is stopping")
+
+
+from car import Car
+
+car1 = Car("Chevy", "Corvette", 2020, "black")
+
+print(car1.make)
+print(car1.model)
+print(car1.year)
+print(car1.color)
+
+car1.drive()
+car1.stop()
+
