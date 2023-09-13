5be890e80289d1bbe78f7342aa4dab5b47b28696
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-09-13 19:51:55 +0800

    Create addset.py

---
+++ addset.py
@@ -0,0 +1,27 @@
+#Sets
+#Access
+set1 = {"A","B","C"}
+set2 = {"W","X","Y"}
+list1 = [1,2,3,4,5]
+
+print(set1)
+print(len(set1))
+
+print(type(set1))
+
+for x in set1:
+    print(x)
+
+print("B" in set1)
+
+#Add set
+
+set1.add("D")
+print(set1)
+
+set1.update(set2)
+print(set1)
+
+
+set1.update(list1) #adding sets to list or vice versa
+print(set1)
